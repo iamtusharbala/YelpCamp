a4bdc8304267d24bee18af4662b4c231f776eb59
Tushar Balakrishnan <tusharbala284@gmail.com>
2023-12-22 05:06:39 +0530

    added error handling functions

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan')
 const Campground = require('./models/campgrounds');
 const engine = require('ejs-mate');
 const methodOverride = require('method-override');
+const AppError = require('./AppError')
 
 
 app.engine("ejs", engine);
@@ -20,61 +21,92 @@ app.get("/", (req, res) => {
 })
 
 
-app.get("/campgrounds", async (req, res) => {
-    const campgrounds = await Campground.find();
-    res.render("campgrounds/all", { campgrounds });
+app.get("/campgrounds", async (req, res, next) => {
+    try {
+        const campgrounds = await Campground.find();
+        res.render("campgrounds/all", { campgrounds });
+    } catch (error) {
+        next(error)
+    }
 })
 
 app.post("/campgrounds", async (req, res, next) => {
-    console.log(req.body);
-    const { title, location, price, description, image } = req.body;
-    await Campground.insertMany({
-        title,
-        price,
-        image,
-        description,
-        location
-    })
-    console.log('Data entered successfully');
-    res.redirect('/campgrounds')
+    try {
+        console.log(req.body);
+        const { title, location, price, description, image } = req.body;
+        await Campground.insertMany({
+            title,
+            price,
+            image,
+            description,
+            location
+        }, { runValidators: true })
+        console.log('Data entered successfully');
+        res.redirect('/campgrounds')
+    } catch (error) {
+        next(error)
+    }
 })
 
 
-app.put("/campgrounds/:id", async (req, res) => {
-    const { title, location } = req.body;
-    const { id } = req.params;
-    await Campground.findByIdAndUpdate(id, {
-        title,
-        location
-    })
-    console.log('Data updated successfully');
-    res.redirect(`/campgrounds/${id}`)
+app.put("/campgrounds/:id", async (req, res, next) => {
+    try {
+        const { title, location } = req.body;
+        const { id } = req.params;
+        await Campground.findByIdAndUpdate(id, {
+            title,
+            location
+        }, { runValidators: true })
+        console.log('Data updated successfully');
+        res.redirect(`/campgrounds/${id}`)
+    } catch (error) {
+        next(error);
+    }
 })
 
 
-app.delete("/campgrounds/:id", async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    console.log('Data deleted successfully');
-    res.redirect('/campgrounds')
+app.delete("/campgrounds/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await Campground.findByIdAndDelete(id);
+        console.log('Data deleted successfully');
+        res.redirect('/campgrounds')
+    } catch (error) {
+        next(error);
+    }
 })
 app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
 })
 
-
-app.get("/campgrounds/:id", async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render("campgrounds/show", { campground });
+app.get("/campgrounds/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const campground = await Campground.findById(id);
+        if (!campground) {
+            throw new AppError('Did not find campground puttar', 404);
+        }
+        res.render("campgrounds/show", { campground });
+    } catch (error) {
+        next(error)
+    }
 })
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render("campgrounds/edit", { campground });
+app.get("/campgrounds/:id/edit", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const campground = await Campground.findById(id);
+        res.render("campgrounds/edit", { campground });
+    } catch (error) {
+        next(error)
+    }
 })
 
+///Error Handler -- put at last
+app.use((err, req, res, next) => {
+    const { message = 'Something happened', status = 500 } = err;
+    res.status(status).send(message);
+})
 
 
 
